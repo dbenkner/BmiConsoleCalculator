2a18b1eff0e232c0c5f77944fcff8a9266bbdca1
David Benkner <d.benkner@gmail.com>
2023-08-18 20:34:20 -0400

    Add project files.

---
+++ BmiConsoleCalculator/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiConsoleCalculator
+{
+    internal class Person
+    {
+        public int height { get; set; }
+        public int weight { get; set; }
+
+        public Person(int height, int weight)
+        {
+            this.height = height;
+            this.weight = weight;
+        }
+        public double getBMI(int height, int weight)
+        {
+            return Math.Round((weight / Math.Pow(height, 2) * 703), 5);
+        }
+    }
+
+
+}
---
+++ BmiConsoleCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using BmiConsoleCalculator;
+
+bool run = true;
+int weight;
+int height;
+do
+{
+    Console.WriteLine("Enter the height in inches");
+    height = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the weight in pounds");
+    weight = Convert.ToInt32(Console.ReadLine());
+    var Person1 = new Person(height, weight);
+    var bmi = Person1.getBMI(height, weight);
+    Console.WriteLine($"The height is {Person1.height} and the weight is {Person1.weight} and the bmi {bmi}");
+    run = Quit();
+} while (run == true);
+bool Quit()
+{
+    string quit;
+    var dontQuit = true;
+    Console.Write("Do you want to quit Y/N");
+    quit = Console.ReadLine();
+    if (quit == "Y" || quit == "y")
+    {
+        dontQuit = false;
+    }
+    else if (quit != "Y" && quit != "y" && quit != "N" && quit != "n")
+    {
+        Console.Write("Please enter a Y or N");
+        dontQuit = Quit();
+    }
+    return dontQuit;
+}
